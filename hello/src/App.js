ed16521083351b0b96f8ae0c77d504fb10adf130
innopro22 <kisekkainnocent22@gmail.com>
2024-11-20 15:32:46 +0300

    introduction to props and useState

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,22 +1,28 @@
-
 import './App.css';
+import Employee from './componets/employees';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('teacter');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (  
+        <>
+        <input type='text' onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+        }} />
+          <Employee  name="Innopro" role="hacker"/>
+          <Employee name="Hamza" role={role}/>
+          <Employee name="cate" />
+
+        </>
+      )
+    
+      : (
+        <p>you cannot see the employees</p>
+      )}
     </div>
   );
 }
